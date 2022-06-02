22a91be06fbe1a04981b47acb456c5d227d1f8ad
Mikie0520 <cano1959685@gmail.com>
2022-06-02 18:13:12 +0800

    Update compression.py

--- connector/compression.py
+++ connector/compression.py
@@ -19,7 +19,6 @@ class Compression:
         # TODO:
         # check slope(new_point, archived point) is safe or not
         # if slope
-        print("done")
         save_point = self.buffer.save_snapshot()
         self.buffer.push_new_point(new_point)
         return save_point
