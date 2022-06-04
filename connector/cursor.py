21d449df0b408710079a1c0511cd7920bf2b2bc5
bcy <btbcy@users.noreply.github.com>
2022-06-04 11:28:08 +0800

    split custom_select into select_many and select_one

--- connector/cursor.py
+++ connector/cursor.py
@@ -87,29 +87,20 @@ class Cursor(MySQLCursor):
 
     def _custom_select(self, stmt: str):
         """
-        TODO
-        set self._select_flag = True
+
         store interpolated result from select_interpolation to
         self._selected_row_generator
         """
-        table_name = re.search(r"from\s(\w+)", stmt).group(1)
-
-        test_point1 = DataPoint(datetime.datetime.now(), -999)
-        test_point2 = DataPoint(
-            datetime.datetime.now() + datetime.timedelta(minutes=8), -1999)
-
-        # TODO handle if table_name not in compression_dict.keys()
-        comp = self.compression_dict[table_name]
-        self._selected_row_generator = comp.select_interpolation(
-            datetime.datetime.now(),
-            [test_point1, test_point2])
-
-        return
+        if ("<" in stmt
+                or ">" in stmt
+                or "where" not in stmt):
+            self._handle_select_many(stmt)
+        else:
+            self._handle_select_one(stmt)
 
     def _custom_create_table(self, stmt: str):
         """
 
-        TODO
         Assume that dev_margin = xxx only appears at the last part
         seperated by a comma
         For example,
@@ -154,6 +145,27 @@ class Cursor(MySQLCursor):
         result = [(pnt.timestamp, pnt.value)
                   for pnt in self._selected_row_generator]
         return result
+
+    def _handle_select_one(self, stmt):
+        # TODO
+        table_name = re.search(r"from\s(\w+)", stmt).group(1)
+
+        test_point1 = DataPoint(datetime.datetime.now(), -999)
+        test_point2 = DataPoint(
+            datetime.datetime.now() + datetime.timedelta(minutes=8), -1999)
+
+        # TODO if have time
+        # handle if table_name not in compression_dict.keys()
+        comp = self.compression_dict[table_name]
+        self._selected_row_generator = comp.select_interpolation(
+            datetime.datetime.now(),
+            (test_point1, test_point2)
+        )
+
+    def _handle_select_many(self, stmt):
+        # TODO
+        self._selected_row_generator = (
+            DataPoint(datetime.datetime.now(), -999 * i) for i in range(3))
 
     def _create_dev_margin_table_if_not_exists(self):
         stmt_creat_table = (
